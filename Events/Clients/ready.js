eb6e89fc677c14bfc06f566f492c651f5a65c7dd
SmithWebDev <smithwebdd@gmail.com>
2022-02-15 13:28:04 -0500

    added mongodb connection to the bot, confirmed no errors

--- Events/Clients/ready.js
+++ Events/Clients/ready.js
@@ -1,10 +1,22 @@
 const { Client } = require('discord.js');
+const mongoose = require('mongoose');
+const { Database } = require('../../botconfig.json');
 
 module.exports = {
   name: 'ready',
   once: true,
   execute(client) {
     console.log("the Client is now ready")
-    client.user.setActivity("Server", {type: "WATCHING"})
+    client.user.setActivity("Server", {type: "WATCHING"});
+
+    if(!Database) return;
+    mongoose.connect(Database, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    }).then(() => {
+      console.log("The client is now connected to the database.")
+    }).catch((err) => {
+      console.log(err)
+    });
   }
 }
